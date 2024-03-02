d4175a31721812fe3a43fcd0db052333f7b74929
ajaykandpal <ajay.kandpal1998@gmail.com>
2024-03-02 19:33:27 +0530

    adding appointment list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
 import React from 'react';
+import { useGetAppointmentListQuery } from './api/appointmentApi';
+
+import { match } from 'ts-pattern';
+import { Loader } from './components/Loader';
+import { AppointmentTable } from './components/AppointmentTable';
 
 function App() {
+  const { data, isError, isFetching, isSuccess } = useGetAppointmentListQuery();
   return (
-    <div className='bg-black h-screen'>
-      <h1 className='text-white'>hello there</h1>
-    </div>
+    <>
+      {match({ isError, isFetching, isSuccess })
+        .with({ isError: true }, () => (
+          <div className='w-screen h-screen flex justify-center items-center'>
+            <h3>Something went wrong, Please try later.</h3>
+          </div>
+        ))
+        .with({ isFetching: true }, () => (
+          <div className='w-screen h-screen flex justify-center items-center'>
+            <Loader />
+          </div>
+        ))
+        .with({ isSuccess: true }, () => <AppointmentTable appointments={data.appointments} />)
+        .otherwise(() => (
+          <>Something unexpected happened. Please contact admin.</>
+        ))}
+    </>
   );
 }
 
