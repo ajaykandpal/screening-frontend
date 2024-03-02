36ca9300f5f804351eb7771a724fafdb984d2af1
ajaykandpal <ajay.kandpal1998@gmail.com>
2024-03-02 16:40:59 +0530

    setting up API

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { appointmentApi } from './api/appointmentApi';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={appointmentApi}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
