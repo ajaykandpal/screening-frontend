ce62dddf39559b25f0f049feaf1640b50778bf21
ajaykandpal <ajay.kandpal1998@gmail.com>
2024-03-02 15:08:46 +0530

    lint, prettier and husky hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="bg-black h-screen">
+    <div className='bg-black h-screen'>
       <h1 className='text-white'>hello there</h1>
     </div>
   );
